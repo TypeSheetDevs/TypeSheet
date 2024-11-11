eb934e4012569adc49fa57ef74cc671ffd696756
MiloszS <m.sobczyk.official@gmail.com>
2024-11-11 17:06:33 +0100

    Add basic draggable functionality

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,58 +1,83 @@
 import './TopBar.styles.css';
+import { useState, DragEvent } from 'react';
 import { topBarColor } from '../../data/config';
 import ButtonsGroupProps from '@components/PropsInterfaces/ButtonsGroupProps';
 import ButtonsGroup from '@components/ButtonsGroup/ButtonsGroup';
 
 function TopBar() {
-  /* TODO: config extraction */
-  let logoButtonGroup: ButtonsGroupProps = {
-    buttons: [
-      {
-        iconPath: '../../assets/icons/music_note.svg',
-        onClick: () => {
-          console.log('logo');
+  const [buttonsGroupArray, setButtonsGroupArray] = useState<ButtonsGroupProps[]>([
+    {
+      buttons: [
+        {
+          iconPath: '../../assets/icons/music_note.svg',
+          onClick: () => {
+            console.log('logo');
+          },
         },
-      },
-    ],
+      ],
+    },
+    {
+      buttons: [
+        {
+          iconPath: '../../assets/icons/skip_previous.svg',
+          onClick: () => {
+            console.log('skip previous');
+          },
+        },
+        {
+          iconPath: '../../assets/icons/play_arrow.svg',
+          onClick: () => {
+            console.log('play');
+          },
+        },
+        {
+          iconPath: '../../assets/icons/skip_next.svg',
+          onClick: () => {
+            console.log('skip next');
+          },
+        },
+      ],
+    },
+  ]);
+
+  const [draggedIndex, setDraggedIndex] = useState<number | null>(null);
+
+  const handleDragStart = (index: number) => {
+    setDraggedIndex(index);
   };
 
-  let playButtonsGroup: ButtonsGroupProps = {
-    buttons: [
-      {
-        iconPath: '../../assets/icons/skip_previous.svg',
-        onClick: () => {
-          console.log('skip previous');
-        },
-      },
-      {
-        iconPath: '../../assets/icons/play_arrow.svg',
-        onClick: () => {
-          console.log('play');
-        },
-      },
-      {
-        iconPath: '../../assets/icons/skip_next.svg',
-        onClick: () => {
-          console.log('skip next');
-        },
-      },
-    ],
+  const handleDragOver = (event: DragEvent<HTMLDivElement>, _: number) => {
+    event.preventDefault();
   };
 
-  let buttonsGroupArray: ButtonsGroupProps[] = [logoButtonGroup, playButtonsGroup];
+  const handleDrop = (event: DragEvent<HTMLDivElement>, index: number) => {
+    event.preventDefault();
+    if (draggedIndex !== null && draggedIndex !== index) {
+      const updatedButtonsGroupArray = [...buttonsGroupArray];
+      const [draggedItem] = updatedButtonsGroupArray.splice(draggedIndex, 1);
+      updatedButtonsGroupArray.splice(index, 0, draggedItem);
+
+      setButtonsGroupArray(updatedButtonsGroupArray);
+    }
+
+    setDraggedIndex(null);
+  };
 
   return (
     <div
       className="top-bar"
       style={{ backgroundColor: topBarColor }}>
       {buttonsGroupArray.map((buttonsGroup, index) => (
-        <>
-          <ButtonsGroup
-            key={index}
-            buttons={buttonsGroup.buttons}
-          />
+        <div
+          className="draggable-component"
+          key={index}
+          draggable
+          onDragStart={() => handleDragStart(index)}
+          onDragOver={event => handleDragOver(event, index)}
+          onDrop={event => handleDrop(event, index)}>
+          <ButtonsGroup buttons={buttonsGroup.buttons} />
           {index < buttonsGroupArray.length - 1 && <div className="separator" />}
-        </>
+        </div>
       ))}
     </div>
   );
