0544f861f15dd229dd3d71043626c14b17c77aab
portalion <77tobik77@gmail.com>
2024-11-15 19:51:10 +0100

    move everything to custom hook

--- src/layouts/ScrollableView/ScrollableView.tsx
+++ src/layouts/ScrollableView/ScrollableView.tsx
@@ -1,42 +1,12 @@
-import NoteViewRenderer from '@components/NoteViewRenderer/NoteViewRenderer';
 import styles from './ScrollableView.styles.module.css';
-import { NotationRenderer } from '@services/notationRenderer/NotationRenderer';
 import NoteViewSVGRenderer from '@components/NoteViewSVGRenderer/NoteViewSVGRenderer';
-import { useEffect, useRef, useState } from 'react';
-import EventNotifier from '@services/eventNotifier/eventNotifier';
+import { useRef } from 'react';
+import useScrollBox from './useScrollBox';
 
 function ScrollableView() {
-  const scrollableBox = useRef<HTMLDivElement>(null!);
-  const [scrollHeight, setScrollHeight] = useState(0);
-  const [first, setFirst] = useState(0);
-  const [visible, setVisible] = useState(0);
-  const [containerHeight, setContainerHeight] = useState(0);
-  useEffect(() => {
-    const handleScroll = () => {
-      if (scrollableBox.current) {
-        let staveHeight = NotationRenderer.getInstance().StaveHeight;
-        let firtStave = Math.floor(scrollableBox.current.scrollTop / staveHeight);
-        let viewportHeight = scrollableBox.current.clientHeight;
-        let visibleStaves = viewportHeight / staveHeight;
-        setVisible(visibleStaves);
-        setScrollHeight(firtStave * staveHeight);
-        setFirst(firtStave);
-        setContainerHeight(staveHeight * NotationRenderer.getInstance().staves.length);
-      }
-    };
-    if (scrollableBox.current) {
-      handleScroll();
-      scrollableBox.current.addEventListener('scroll', handleScroll);
-      EventNotifier.AddListener('numberOfStavesChanged', handleScroll);
-    }
-
-    return () => {
-      if (scrollableBox.current) {
-        scrollableBox.current.removeEventListener('scroll', handleScroll);
-        EventNotifier.RemoveListener('numberOfStavesChanged', handleScroll);
-      }
-    };
-  }, []);
+  const scrollableBox = useRef<HTMLDivElement>(null);
+  const [containerHeight, stavesStartingHeight, firstStaveIndex, visibleStavesCount] =
+    useScrollBox(scrollableBox);
 
   return (
     <div
@@ -49,9 +19,9 @@ function ScrollableView() {
           flex: '1 1 0',
         }}>
         <NoteViewSVGRenderer
-          lastStaveIndex={first + visible + 1}
-          startingHeight={scrollHeight}
-          startingStaveIndex={first}
+          startingHeight={stavesStartingHeight}
+          startingStaveIndex={firstStaveIndex}
+          lastStaveIndex={firstStaveIndex + visibleStavesCount + 3}
         />
       </div>
     </div>
