f09d87d707e1df00451b458f9b070ef50048f7ac
portalion <77tobik77@gmail.com>
2024-11-16 13:24:00 +0100

    add test configuration

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+module.exports = {
+    testEnvironment: 'jsdom',
+    moduleNameMapper: {
+        '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+        '^.+\\.svg$': 'jest-transformer-svg',
+        '^@assets/(.*)': '<rootDir>/src/assets/$1',
+        '^@components/(.*)': '<rootDir>/src/components/$1',
+        '^@context/(.*)': '<rootDir>/src/context/$1',
+        '^@data/(.*)': '<rootDir>/src/data/$1',
+        '^@hooks/(.*)': '<rootDir>/src/hooks/$1',
+        '^@layouts/(.*)': '<rootDir>/src/layouts/$1',
+        '^@utils/(.*)': '<rootDir>/src/utils/$1',
+        '^@services/(.*)': '<rootDir>/src/services/$1',
+    },
+    transform: {
+        '^.+.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.web.json' }],
+    },
+};
+
