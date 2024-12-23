1f381665b3d8036b38464158fee6f4fc706c1860
MiloszS <m.sobczyk.official@gmail.com>
2024-12-23 19:20:48 +0100

    Add ConfigManager.types.ts

---
+++ src/components/ConfigManager/ConfigManager.types.ts
@@ -0,0 +1,63 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { SavedParameterName, ValueOf } from '@services/ConfigService/ConfigService.types';
+import { ViewType } from '@layouts/MainView/MainView.types';
+
+export enum EditorType {
+    Text,
+    Number,
+    Color,
+    Select,
+}
+
+export type Validator<T> = (value: T) => boolean;
+
+export interface EditorConfig<T> {
+    editorType: EditorType;
+    validators?: Validator<T>[];
+    extraParams?: { [key: string]: any };
+}
+
+export type EditorConfigMapType = {
+    [key in SavedParameterName]: EditorConfig<ValueOf<key>>;
+};
+
+export const EditorConfigMap: EditorConfigMapType = {
+    [SavedParameterName.StartingView]: {
+        editorType: EditorType.Select,
+        extraParams: {
+            options: [ViewType.Paged, ViewType.Scrollable],
+        },
+    },
+    [SavedParameterName.BarsPerStave]: {
+        editorType: EditorType.Number,
+        extraParams: {
+            min: 1,
+            max: 10,
+        },
+    },
+    [SavedParameterName.StaveMinimumHeightDistance]: {
+        editorType: EditorType.Number,
+        extraParams: {
+            min: 20,
+            max: 60,
+        },
+    },
+    [SavedParameterName.MainViewMargin]: {
+        editorType: EditorType.Number,
+        extraParams: {
+            min: 5,
+            max: 20,
+        },
+    },
+    [SavedParameterName.StavesPerPage]: {
+        editorType: EditorType.Number,
+        extraParams: {
+            min: 3,
+            max: 8,
+        },
+    },
+    [SavedParameterName.TopBarColor]: {
+        editorType: EditorType.Color,
+    },
+};
