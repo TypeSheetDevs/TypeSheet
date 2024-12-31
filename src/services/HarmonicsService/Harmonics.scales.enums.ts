5030b7a919433d39202ed8ba72015f973cd3bc65
MiloszS <m.sobczyk.official@gmail.com>
2024-12-31 21:09:02 +0100

    Checkpoint

--- src/services/HarmonicsService/Harmonics.scales.enums.ts
+++ src/services/HarmonicsService/Harmonics.scales.enums.ts
@@ -1,7 +1,9 @@
-export enum Scale {
+export enum Mode {
+    // Major
     Ionian = 'Ionian',
     Lydian = 'Lydian',
     Mixolydian = 'Mixolydian',
+    // Minor
     Aeolian = 'Aeolian',
     Dorian = 'Dorian',
     Phrygian = 'Phrygian',
@@ -10,14 +12,31 @@ export enum Scale {
     MelodicMinor = 'Melodic Minor',
 }
 
-export const ScalePatterns: Record<Scale, number[]> = {
-    [Scale.Ionian]: [2, 2, 1, 2, 2, 2, 1],
-    [Scale.Lydian]: [2, 2, 2, 1, 2, 2, 1],
-    [Scale.Mixolydian]: [2, 2, 1, 2, 2, 1, 2],
-    [Scale.Aeolian]: [2, 1, 2, 2, 1, 2, 2],
-    [Scale.Dorian]: [2, 1, 2, 2, 2, 1, 2],
-    [Scale.Phrygian]: [1, 2, 2, 2, 1, 2, 2],
-    [Scale.Locrian]: [1, 2, 2, 1, 2, 2, 2],
-    [Scale.HarmonicMinor]: [2, 1, 2, 2, 1, 3, 1],
-    [Scale.MelodicMinor]: [2, 1, 2, 2, 2, 2, 1],
+export type ModeType = 'minor' | 'major';
+
+export const ModeTypes: Record<Mode, ModeType> = {
+    [Mode.Ionian]: 'major',
+    [Mode.Lydian]: 'major',
+    [Mode.Mixolydian]: 'major',
+    [Mode.Aeolian]: 'minor',
+    [Mode.Dorian]: 'minor',
+    [Mode.Phrygian]: 'minor',
+    [Mode.Locrian]: 'minor',
+    [Mode.HarmonicMinor]: 'minor',
+    [Mode.MelodicMinor]: 'minor',
 };
+
+export const ScalePatterns: Record<Mode, number[]> = {
+    [Mode.Ionian]: [2, 2, 1, 2, 2, 2, 1],
+    [Mode.Lydian]: [2, 2, 2, 1, 2, 2, 1],
+    [Mode.Mixolydian]: [2, 2, 1, 2, 2, 1, 2],
+    [Mode.Aeolian]: [2, 1, 2, 2, 1, 2, 2],
+    [Mode.Dorian]: [2, 1, 2, 2, 2, 1, 2],
+    [Mode.Phrygian]: [1, 2, 2, 2, 1, 2, 2],
+    [Mode.Locrian]: [1, 2, 2, 1, 2, 2, 2],
+    [Mode.HarmonicMinor]: [2, 1, 2, 2, 1, 3, 1],
+    [Mode.MelodicMinor]: [2, 1, 2, 2, 2, 2, 1],
+};
+
+export const NotesSharps = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'B#'];
+export const NotesFlats = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B', 'Cb'];
--- src/services/HarmonicsService/HarmonicsService.ts
+++ src/services/HarmonicsService/HarmonicsService.ts
@@ -1,8 +1,15 @@
 import { chord } from 'music21j';
 import { ChordType, ChordTypeMusic21 } from '@services/HarmonicsService/Harmonics.chords.enums';
 import { ChordInfo } from '@services/HarmonicsService/Harmonics.types';
+import {
+    NotesFlats,
+    NotesSharps,
+    Mode,
+    ScalePatterns,
+} from '@services/HarmonicsService/Harmonics.scales.enums';
 
 export class HarmonicsService {
+    // Chords
     public static GetChordInfo(keys: string[]): ChordInfo {
         const c = new chord.Chord(keys);
 
@@ -31,5 +38,33 @@ export class HarmonicsService {
         const entry = Object.entries(ChordTypeMusic21).find(([_, value]) => value === chordName);
 
         return entry ? (entry[0] as ChordType) : ChordType.Other;
+    }
+
+    // Scales
+    public static GenerateScale(tonic: string, mode: Mode, useSharps?: boolean): string[] {
+        let notes: string[];
+
+        if (useSharps === undefined)
+            notes = tonic.endsWith('b') || tonic === 'F' ? NotesFlats : NotesSharps;
+        else notes = useSharps ? NotesSharps : NotesFlats;
+
+        const scale: string[] = [];
+        let index = notes.indexOf(tonic);
+
+        if (index === -1) throw new Error(`Invalid tonic: ${tonic}`);
+
+        for (const step of ScalePatterns[mode]) {
+            scale.push(notes[index]);
+            index = (index + step) % notes.length;
+        }
+        this.UseSharps(tonic, mode);
+        return scale;
+    }
+
+    private static UseSharps(tonic: string, mode: Mode): boolean {
+        if (tonic.indexOf('#') !== -1) return true;
+        else if (tonic.indexOf('b') !== -1) return false;
+
+        return true;
     }
 }
