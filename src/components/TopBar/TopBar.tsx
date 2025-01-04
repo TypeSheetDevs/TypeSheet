ec40c387c171ce38e3a40f980197b0aef54c57ed
portalion <77tobik77@gmail.com>
2025-01-04 06:06:13 +0100

    refactor TopBar.tsx

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,14 +1,13 @@
 import styles from './TopBar.styles.module.css';
-import { DragEvent, useState } from 'react';
 import { ConfigService } from '@services/ConfigService/ConfigService';
-import ButtonsGroup from '@components/ButtonsGroup/ButtonsGroup';
+import ButtonsGroup from './ButtonsGroup/ButtonsGroup';
 import getButtonIcon from '@assets/icons/getIcon';
 import { SavedParameterName } from '@services/ConfigService/ConfigService.types';
-import OpenCloseButton from '@components/TopBar/OpenCloseButton';
+import OpenCloseButton from './OpenCloseButton/OpenCloseButton';
 import EventNotifier from '@services/eventNotifier/eventNotifier';
 
 function TopBar() {
-  const [buttonsGroups, setButtonsGroups] = useState<ButtonsGroupType[]>([
+  const buttonsGroups = [
     {
       buttons: [
         {
@@ -50,55 +49,23 @@ function TopBar() {
         },
       ],
     },
-  ]);
-  const [draggedIndex, setDraggedIndex] = useState<number | null>(null);
-
+  ];
   const topBarColor = ConfigService.getInstance().getValue(SavedParameterName.TopBarColor);
-
-  const handleDragStart = (index: number) => {
-    setDraggedIndex(index);
-  };
-
-  const handleDragOver = (event: DragEvent<HTMLDivElement>, index: number) => {
-    if (index !== 0) {
-      event.preventDefault();
-    }
-  };
-
-  const handleDrop = (event: DragEvent<HTMLDivElement>, index: number) => {
-    event.preventDefault();
-    if (draggedIndex !== null && draggedIndex !== index && index !== 0) {
-      const updatedButtonsGroupArray = [...buttonsGroups];
-      const [draggedItem] = updatedButtonsGroupArray.splice(draggedIndex, 1);
-      updatedButtonsGroupArray.splice(index, 0, draggedItem);
-      setButtonsGroups(updatedButtonsGroupArray);
-    }
-    setDraggedIndex(null);
-  };
 
   return (
     <div
       className={styles.topBar}
       style={{ backgroundColor: topBarColor }}
       data-testid="top-bar">
-      {buttonsGroups.map((buttonsGroup, index) => (
-        <div
-          key={index}
-          className={styles.groupContainer}>
-          <div
-            className={styles.draggableComponent}
-            data-testid={`draggable-component-${index}`}
-            draggable={index !== 0}
-            onDragStart={() => handleDragStart(index)}
-            onDragOver={event => handleDragOver(event, index)}
-            onDrop={event => handleDrop(event, index)}>
-            <ButtonsGroup buttons={buttonsGroup.buttons} />
-          </div>
-          {index < buttonsGroups.length - 1 && <div className={styles.separator} />}
-        </div>
+      {buttonsGroups.map((buttonsGroup, buttonsGroupIndex) => (
+        <ButtonsGroup
+          key={buttonsGroupIndex}
+          buttons={buttonsGroup.buttons}
+          isLast={buttonsGroupIndex < buttonsGroups.length - 1}
+        />
       ))}
       <OpenCloseButton
-        style={{ marginLeft: 'auto' }}
+        style={{ marginLeft: 'auto', order: 100 }}
         onToggle={() => EventNotifier.Notify('toggleConfigManager')}
       />
     </div>
