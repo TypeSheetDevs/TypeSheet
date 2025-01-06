bd8b9e6e348bdc039f267f01f6a9bff4d126233c
MiloszS <m.sobczyk.official@gmail.com>
2025-01-06 21:21:28 +0100

    Add styling to MidiSelector

--- src/components/MidiSelector/MidiSelector.tsx
+++ src/components/MidiSelector/MidiSelector.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { MidiService } from '@services/MidiService/MidiService';
 import styles from './MidiSelector.styles.module.css';
+import getButtonIcon from '@assets/icons/getIcon';
 
 function MidiSelector() {
   const midiService = MidiService.getInstance();
@@ -12,25 +13,45 @@ function MidiSelector() {
     midiService.ConnectToPort(portName);
   };
 
+  const handleDisconnect = () => {
+    midiService.Disconnect();
+    setSelectedPort(null);
+  };
+
   return (
     <div className={styles.container}>
       <h3 className={styles.header}>Select MIDI Port</h3>
-      <div>
+      <div className={styles.ports}>
         {ports.map(port => (
-          <div
+          <button
+            className={styles.portButton}
             key={port.name}
-            className={styles.listItem}>
-            <input
-              type="radio"
-              value={port.name}
-              className={styles.radioInput}
-              checked={selectedPort === port.name}
-              onChange={() => handlePortSelection(port.name)}
+            onClick={() => handlePortSelection(port.name)}>
+            <img
+              src={getButtonIcon(
+                selectedPort === port.name
+                  ? 'radio_button_checked.svg'
+                  : 'radio_button_unchecked.svg',
+              )}
+              alt={'radio button'}
+              className={styles.portButtonImg}
+              style={{
+                filter:
+                  selectedPort === port.name
+                    ? 'brightness(40%) saturate(0%)'
+                    : 'brightness(70%) saturate(0%)',
+              }}
             />
-            <span className={styles.label}>{port.name}</span>
-          </div>
+            <span>{port.name}</span>
+          </button>
         ))}
       </div>
+      <button
+        className={styles.disconnectButton}
+        onClick={handleDisconnect}
+        disabled={!selectedPort}>
+        Disconnect
+      </button>
     </div>
   );
 }
