a70269b6a841a90472e4d16761dbe08950649176
MiloszSobczyk <m.sobczyk.official@gmail.com>
2025-01-18 17:39:41 +0100

    Add Midi connector

--- src/components/TopBar/ContentTooltip/Tooltip.tsx
+++ src/components/TopBar/ContentTooltip/Tooltip.tsx
@@ -1,50 +1,15 @@
-import { useEffect, useRef, useState } from 'react';
+import styles from './Tooltip.styles.module.css';
 
 function Tooltip({ iconPath, content: Content }: TooltipProps) {
-  const [isVisible, setIsVisible] = useState(false);
-  const tooltipRef = useRef<HTMLDivElement>(null);
-  const buttonRef = useRef<HTMLButtonElement>(null);
-
-  const handleClickOutside = (event: MouseEvent) => {
-    if (
-      tooltipRef.current &&
-      !tooltipRef.current.contains(event.target as Node) &&
-      buttonRef.current &&
-      !buttonRef.current.contains(event.target as Node)
-    ) {
-      setIsVisible(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener('click', handleClickOutside);
-    return () => {
-      document.removeEventListener('click', handleClickOutside);
-    };
-  }, []);
-
   return (
-    <div style={{ position: 'relative' }}>
-      <button
-        ref={buttonRef}
-        onClick={() => setIsVisible(!isVisible)}>
-        <img
-          src={iconPath}
-          alt="tooltip icon"
-        />
-      </button>
-      {isVisible && (
-        <div
-          ref={tooltipRef}
-          style={{
-            position: 'absolute',
-            top: '100%',
-            left: '50%',
-            transform: 'translateX(-50%)',
-          }}>
-          <Content />
-        </div>
-      )}
+    <div className={styles.button}>
+      <img
+        draggable={false}
+        src={iconPath}
+      />
+      <div className={styles.tooltip}>
+        <Content />
+      </div>
     </div>
   );
 }
