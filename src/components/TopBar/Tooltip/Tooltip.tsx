ab1303a3f6c6e18dc0fd12d94d78e4151dc73516
MiloszSobczyk <m.sobczyk.official@gmail.com>
2025-01-24 20:22:22 +0100

    Reapply "Change styling"
    This reverts commit 1f8109912cf738a52f74aa17ab3a87600572660e.

--- src/components/TopBar/Tooltip/Tooltip.tsx
+++ src/components/TopBar/Tooltip/Tooltip.tsx
@@ -1,13 +1,17 @@
 import styles from './Tooltip.styles.module.css';
 
-function Tooltip({ iconPath, content }: TooltipProps) {
+function Tooltip({ iconPath, content, usedPadding }: TooltipProps) {
   return (
     <div className={styles.button}>
       <img
         draggable={false}
         src={iconPath}
       />
-      <div className={styles.tooltip}>{content}</div>
+      <div
+        className={styles.tooltip}
+        style={{ padding: usedPadding }}>
+        {content}
+      </div>
     </div>
   );
 }
